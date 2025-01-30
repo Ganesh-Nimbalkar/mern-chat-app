46d836a36cf297702374ba507df1a9b27e5354fa
Ganesh <ganeshnimbalkar40890@gmail.com>
2025-01-30 12:41:52 +0530

    updated url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://mern-chat-app-24m3.onrender.com/login", {
+			const socket = io("https://mern-chat-app-24m3.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
